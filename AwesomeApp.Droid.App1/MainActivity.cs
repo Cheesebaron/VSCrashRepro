898687a79de1d8b7bebd2b4053d096f222fe1f1d
Tomasz Cielecki <kraken@ostebaronen.dk>
2018-03-07 19:04:34 +0100

    Added dup layout repro

---
+++ AwesomeApp.Droid.App1/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using AwesomeApp.Droid.Views;
+using Android.Content;
+
+namespace AwesomeApp.Droid.App1
+{
+    [Activity(Label = "App1", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var button = FindViewById<Button>(Resource.Id.button);
+            button.Click += (s, e) =>
+            {
+                var intent = new Intent(this, typeof(WelcomeView));
+                StartActivity(intent);
+            };
+        }
+    }
+}
+
---
+++ AwesomeApp.Droid.App2/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using AwesomeApp.Droid.Views;
+
+namespace AwesomeApp.Droid.App2
+{
+    [Activity(Label = "App2", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var button = FindViewById<Button>(Resource.Id.button);
+            button.Click += (s, e) =>
+            {
+                var intent = new Intent(this, typeof(WelcomeView));
+                StartActivity(intent);
+            };
+        }
+    }
+}
+
